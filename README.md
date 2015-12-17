125f7121f5fd684f938a553a114e130425b5f55f
Atsushi Kojo <artsnet111@gmail.com>
2015-12-17 18:50:47 +0900

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+node-red-contrib-milkcocoa
+========================
+A collection of <a href="http://nodered.org" target="_new">Node-RED</a> nodes for <a href="https://mlkcca.com/" target="_new">Milkcocoa</a>.
+
+[![NPM](https://nodei.co/npm/node-red-contrib-milkcocoa.png?downloads=true)](https://nodei.co/npm/node-red-contrib-milkcocoa/)
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-milkcocoa
+
+Acknowledgements
+----------------
+
+The node-red-contrib-milkcocoa uses the following open source software:
+
+- [milkcocoa] (https://www.npmjs.com/package/milkcocoa): milkcocoa client for Node.js.
+
+License
+-------
+
+See [license] (https://github.com/joeartsea/node-red-contrib-milkcocoa/blob/master/LICENSE) (Apache License Version 2.0).
---
+++ milkcocoa.js
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2015 Atsushi Kojo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function (RED) {
+  'use strict';
+  var MilkCocoa = require('milkcocoa');
+
+  function MilkcocoaNode (n) {
+    RED.nodes.createNode(this, n);
+  }
+
+  RED.nodes.registerType('milkcocoa', MilkcocoaNode, {
+    credentials: {
+      appId: { type: 'text' },
+      apiKey: { type: 'text' },
+      apiSecret: { type: 'password' }
+    }
+  });
+
+  function MilkcocaInNode (n) {
+    RED.nodes.createNode(this, n);
+    this.milkcocoa = n.milkcocoa;
+    this.dataStore = n.dataStore;
+    this.operation = n.operation;
+    this.milkcocoaConfig = RED.nodes.getNode(this.milkcocoa);
+    if (this.milkcocoaConfig) {
+      var node = this;
+      var milkcocoa;
+      var credentials = RED.nodes.getCredentials(this.milkcocoa);
+      if (credentials.apiKey && credentials.apiSecret) {
+        milkcocoa = MilkCocoa.connectWithApiKey(credentials.appId + '.mlkcca.com', credentials.apiKey, credentials.apiSecret);
+      } else {
+        milkcocoa = new MilkCocoa(credentials.appId + '.mlkcca.com');
+      }
+      var ds = milkcocoa.dataStore(node.dataStore);
+      ds.on(node.operation, function (res) {
+        var msg = {};
+        msg.payload = res;
+        node.send(msg);
+      });
+      this.on('close', function() {
+        ds.off(node.operation);
+      });
+    }
+  }
+
+  RED.nodes.registerType("milkcocoa in", MilkcocaInNode);
+
+  function MilkcocaOutNode (n) {
+    RED.nodes.createNode(this, n);
+    this.milkcocoa = n.milkcocoa;
+    this.dataStore = n.dataStore;
+    this.operation = n.operation;
+    this.targetId = n.targetId;
+    this.milkcocoaConfig = RED.nodes.getNode(this.milkcocoa);
+    if (this.milkcocoaConfig) {
+      var node = this;
+      var milkcocoa;
+      var credentials = RED.nodes.getCredentials(this.milkcocoa);
+      this.on('input', function (msg) {
+        node.sendMsg = function (err, result) {
+          if (err) {
+            node.error(err.toString());
+            node.status({ fill: 'red', shape: 'ring', text: 'failed' });
+          } else {
+            node.status({});
+          }
+          msg.payload = result;
+          node.send(msg);
+        };
+        if (credentials.apiKey && credentials.apiSecret) {
+          milkcocoa = MilkCocoa.connectWithApiKey(credentials.appId + '.mlkcca.com', credentials.apiKey, credentials.apiSecret);
+        } else {
+          milkcocoa = new MilkCocoa(credentials.appId + '.mlkcca.com');
+        }
+        var targetId = msg.targetId || node.targetId;
+        var ds = milkcocoa.dataStore(node.dataStore);
+        var payload = typeof msg.payload === 'string' ? JSON.parse(msg.payload) : msg.payload;
+        switch (node.operation) {
+          case 'push':
+            return ds.push(payload, node.sendMsg);
+            break;
+          case 'send':
+            return ds.send(payload, node.sendMsg);
+            break;
+          case 'set':
+            return ds.set(targetId, payload, node.sendMsg);
+            break;
+          case 'remove':
+            return ds.remove(targetId, node.sendMsg);
+            break;
+        };
+      });
+    }
+  }
+
+  RED.nodes.registerType("milkcocoa out", MilkcocaOutNode);
+}
